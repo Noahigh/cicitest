127d21a0864e237b2798c10ea39725b3681ac9c8
Noahigh <806446288@qq.com>
2017-08-30 21:50:58 +0800

    小题-敏感词汇检查
    小题-敏感词汇检查

---
+++ select_words.py
@@ -0,0 +1,36 @@
+"""
+敏感词汇筛选
+找出铭感词汇，并反馈
+"""
+file_url = r'text_test\filtered.txt'
+
+st_string = []
+rs = ''
+with open(file_url) as file_object:
+	lines = file_object.readlines()
+	for line in lines:
+		rs += line
+
+		if('\n' in rs):
+			st_string.append(rs.rstrip())
+			rs = ''
+
+
+print(st_string)
+
+'''单个词的判断'''
+abc = input('Please input a word: ')
+
+if(abc in st_string):
+        print('Freedom')
+else:
+        print('Human Rights')
+
+fgh = input('Please input a word: ')
+'''语句判断'''
+for value in st_string:
+        if(value in fgh):
+            print('freedom')
+        else:
+            print('human rights')
+            
